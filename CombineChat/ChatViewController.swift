6396f9529bc9ba41642a2aee11aadb27bce4b81b
Niklas Holloh <developer@niklasholloh.de>
2019-06-07 14:07:39 -0700

    switched to UIKit

---
+++ CombineChat/ChatViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChatViewController.swift
+//  CombineChat
+//
+//  Created by Niklas Holloh on 07.06.19.
+//  Copyright © 2019 Thom Ritterfeld. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+}
