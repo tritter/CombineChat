bd4d9c7b30c934305ab9ee9ffa8c8d338ab7352e
Niklas Holloh <developer@niklasholloh.de>
2019-06-07 13:11:30 -0700

    view changes

--- CombineChat/ContentView.swift
+++ CombineChat/ContentView.swift
@@ -8,40 +8,56 @@
 
 import SwiftUI
 
-struct ContentView : View {
-    let service = ChatService()
+struct ContentView: View {
+    var service = ChatService()
     
     @Binding var textToSend: String
     @State var messages: [ChatMessage]
     
     func sendText() {
-        service.send(chat: textToSend)
+        service.send(message: textToSend)
         textToSend = ""
     }
     
     var body: some View {
         NavigationView {
             VStack {
+                EmptyView().onAppear {
+                    self.service.delegate = self
+                }
+                
                 List(0...1) { item in
                     Text("Incoming Message")
                 }
+                
                 HStack {
                     TextField($textToSend,
                               placeholder: Text("Message"),
                               onEditingChanged: {_ in }) {
-                                
+                                self.sendText()
                     }
                 }
-            }
-            .padding(.bottom)
-        }.navigationBarTitle(Text("Combine Chat"))
+                }
+                .padding(.bottom)
+                .navigationBarTitle(Text("Combine Chat"))
+        }
+    }
+}
+
+extension ContentView: ChatServiceDelegate {
+    func connectedDevicesChanged(manager: ChatService, connectedDevices: [String]) {
+        
+    }
+    
+    func messageReceived(manager: ChatService, message: ChatMessage) {
+        
     }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView(textToSend: .constant(""))
+        ContentView(textToSend: .constant(""), messages: [])
     }
 }
 #endif
