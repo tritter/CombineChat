31c74ee41b10d9fbe950b365427a1009da4db93f
Thom Ritterfeld <thom@ritterfeld.com>
2019-06-07 13:02:14 -0700

    Added message service

---
+++ CombineChat/ChatMessage.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  CombineChat
+//
+//  Created by Thom Ritterfeld on 6/7/19.
+//  Copyright © 2019 Thom Ritterfeld. All rights reserved.
+//
+
+import Foundation
+
+struct ChatMessage {
+    let message: String
+    let username: String
+}
---
+++ CombineChat/ChatService.swift
@@ -0,0 +1,126 @@
+//
+//  ChatService.swift
+//  CombineChat
+//
+//  Created by Thom Ritterfeld on 6/7/19.
+//  Copyright © 2019 Thom Ritterfeld. All rights reserved.
+//
+
+import Foundation
+import MultipeerConnectivity
+
+protocol ChatServiceDelegate {
+    
+    func connectedDevicesChanged(manager : ChatService, connectedDevices: [String])
+    func messageReceived(manager : ChatService, message: ChatMessage)
+    
+}
+
+class ChatService: NSObject{
+    private let ColorServiceType = "ritter-holloh-chat-service"
+    
+    private let myPeerId = MCPeerID(displayName: UIDevice.current.name)
+    private let serviceAdvertiser : MCNearbyServiceAdvertiser
+    private let serviceBrowser : MCNearbyServiceBrowser
+    
+    var delegate : ChatServiceDelegate?
+    
+    lazy var session : MCSession = {
+        let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: .required)
+        session.delegate = self
+        return session
+    }()
+    
+    override init() {
+        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: ColorServiceType)
+        self.serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: ColorServiceType)
+        
+        super.init()
+        
+        self.serviceAdvertiser.delegate = self
+        self.serviceAdvertiser.startAdvertisingPeer()
+        
+        self.serviceBrowser.delegate = self
+        self.serviceBrowser.startBrowsingForPeers()
+    }
+    
+    deinit {
+        self.serviceAdvertiser.stopAdvertisingPeer()
+        self.serviceBrowser.stopBrowsingForPeers()
+    }
+    
+    func send(chat : ChatMessage) {
+        print("sendingMessage: \(chat.message) to \(session.connectedPeers.count) peers")
+        
+        if session.connectedPeers.count > 0 {
+            do {
+                try self.session.send(chat.message.data(using: .utf8)!, toPeers: session.connectedPeers, with: .reliable)
+            }
+            catch let error {
+                print("Error for sending: \(error)")
+            }
+        }
+        
+    }
+    
+}
+
+extension ChatService : MCNearbyServiceAdvertiserDelegate {
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+        print("didNotStartAdvertisingPeer: \(error)")
+    }
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        print("didReceiveInvitationFromPeer \(peerID)")
+        invitationHandler(true, self.session)
+    }
+    
+}
+
+extension ChatService : MCNearbyServiceBrowserDelegate {
+    
+    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+        print("didNotStartBrowsingForPeers: \(error)")
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        print("foundPeer: \(peerID)")
+        print("invitePeer: \(peerID)")
+        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 10)
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        print("lostPeer: \(peerID)")
+    }
+    
+}
+
+extension ChatService : MCSessionDelegate {
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        print("peer \(peerID) didChangeState: \(state.rawValue)")
+        self.delegate?.connectedDevicesChanged(manager: self, connectedDevices:
+            session.connectedPeers.map{$0.displayName})
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        print("didReceiveData: \(data)")
+        let str = String(data: data, encoding: .utf8)!
+        let message = ChatMessage(message: str, username: peerID.displayName)
+        self.delegate?.messageReceived(manager: self, message: message)
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        print("didReceiveStream")
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        print("didStartReceivingResourceWithName")
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        print("didFinishReceivingResourceWithName")
+    }
+    
+}
