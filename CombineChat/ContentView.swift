5e2192c9b48e8c032053864ec7ee1c5d7ba66488
Niklas Holloh <developer@niklasholloh.de>
2019-06-07 13:05:13 -0700

    WIP

--- CombineChat/ContentView.swift
+++ CombineChat/ContentView.swift
@@ -9,15 +9,39 @@
 import SwiftUI
 
 struct ContentView : View {
+    let service = ChatService()
+    
+    @Binding var textToSend: String
+    @State var messages: [ChatMessage]
+    
+    func sendText() {
+        service.send(chat: textToSend)
+        textToSend = ""
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                List(0...1) { item in
+                    Text("Incoming Message")
+                }
+                HStack {
+                    TextField($textToSend,
+                              placeholder: Text("Message"),
+                              onEditingChanged: {_ in }) {
+                                
+                    }
+                }
+            }
+            .padding(.bottom)
+        }.navigationBarTitle(Text("Combine Chat"))
     }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(textToSend: .constant(""))
     }
 }
 #endif
